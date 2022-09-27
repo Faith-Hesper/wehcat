eea9dce2139df3dcd6599e1c33659221f4ee1457
Faith-Hesper <2310298446@qq.com>
2022-09-27 22:54:50 +0800

    更新

---
+++ js/arrow.js
@@ -0,0 +1,22 @@
+/*
+ * @Author: Faith
+ * @Date: 2022-09-27 22:21
+ * @LastAuthor: Faith
+ * @LastEditTime: 2022-09-27 22:21
+ * @Description:
+ */
+
+const arrow = document.getElementsByClassName('arrow-wrap')
+const van = document.querySelector('.arrow-wrap .van-icon')
+const desc = document.querySelector('.desc-wrap')
+arrow[0].addEventListener('click', (e) => {
+  if (van.className === 'van-icon van-icon-arrow-up') {
+    van.className = 'van-icon van-icon-arrow-down'
+    desc.className = 'desc-wrap desc-wrap-folded'
+  } else {
+    van.className = 'van-icon van-icon-arrow-up'
+    desc.className = 'desc-wrap'
+  }
+
+  // console.log(van.className);
+})
---
+++ js/formData.js
@@ -0,0 +1,7 @@
+/*
+ * @Author: Faith
+ * @Date: 2022-09-27 20:43
+ * @LastAuthor: Faith
+ * @LastEditTime: 2022-09-27 20:43
+ * @Description:
+ */
